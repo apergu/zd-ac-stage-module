d71c309bc59aa86cdd40fc75d0a72eeffe5b73a1
Dhuta P <dhuta_venturo@loyalto.id>
2023-10-24 08:09:02 +0700

    FIX update step

--- app/Http/Controllers/Zendesk/NewDealController.php
+++ app/Http/Controllers/Zendesk/NewDealController.php
@@ -134,7 +134,78 @@ class NewDealController extends Controller
    */
   public function update(Request $request, string $id)
   {
-        //
+    // Expected Request Payload
+    // {
+    //     "deal_id": "162580203_2023-10-22T14:51:01Z",
+    //     "deal_name": "Yoga Corp - Pratama Dhuta",
+    //     "contact_id": 481404536,
+    //     "ac_contact_id": 24,
+    //     "stage_id": 33462408
+    // }
+
+    // Update Contact
+    $contact = Contact::where('ac_contact_id', $request->ac_contact_id)->first();
+    if (!$contact) {
+      return $this->responseOK();
+    }
+
+    // Verify Stages Is Available
+    $zd_stage = ZdStage::where('id', $request->stage_id)->first();
+    if (!$zd_stage) {
+      $this->syncStages();
+      return response()->json([
+        'status' => 'error',
+        'message' => 'ZendDesk: Pipeline > Stage, is not found, please check laravel.log'
+      ]);
+    }
+
+    $ac_stage = AcStage::where('name', $zd_stage->name)->first();
+    if (!$ac_stage) {
+      $this->syncStages();
+      return response()->json([
+        'status' => 'error',
+        'message' => 'ActiveCampaign: ZendDesk: Pipeline > Stage, is not found, please check laravel.log'
+      ]);
+    }
+    Log::debug(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $contact->ac_contact_id);
+    Log::debug([
+      'contact' => [
+        'fieldValues' => [
+          [
+            'field' => 6,
+            'fieldValue' => $ac_stage->name
+          ]
+        ]
+      ]
+    ]);
+
+    // Update AC: Contact > Deal Status
+    $response = Http::withHeaders([
+      'Api-Token' => env('ACTIVECAMPAIGN_API_KEY')
+    ])->put(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $contact->ac_contact_id, [
+      'contact' => [
+        'fieldValues' => [
+          [
+            'field' => 6,
+            'value' => $ac_stage->name
+          ]
+        ]
+      ]
+    ]);
+
+    $res_json = $response->json();
+
+    Log::debug('--- RESPONSE ---');
+    Log::debug(json_encode($res_json, JSON_PRETTY_PRINT));
+
+    // Update deal from active campaign
+    // $ac_deal = $res_json['deal'];
+    // $deal->update([
+    //   'ac_deal_id' => $ac_deal['id'],
+    //   'ac_deal_name' => $ac_deal['title'],
+    // ]);
+
+    return $res_json;
   }
 
   /**

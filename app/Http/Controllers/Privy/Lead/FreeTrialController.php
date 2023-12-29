1f21020c38201ba73cbcfa70a8a54ae1bd2d7b1e
Dhuta P <dhuta_venturo@loyalto.id>
2023-12-29 13:18:17 +0700

    fix

--- app/Http/Controllers/Privy/Lead/FreeTrialController.php
+++ app/Http/Controllers/Privy/Lead/FreeTrialController.php
@@ -39,10 +39,10 @@ class FreetrialController extends Controller
     // Retrieve the validated input...
     $validated = $validator->validated();
 
-    $leadID         = $validated['zd_lead_id'];
+    $leadID = $validated['zd_lead_id'];
     $payloadRequest = $request->toArray();
     // Setup payload data
-    $payload        = $this->_setUpLeadOnChangePayload($leadID, $payloadRequest);
+    $payload = $this->_setUpLeadOnChangePayload($leadID, $payloadRequest);
     if (!$payload) {
       return response()->json([
         'action' => 'updateLeads',
@@ -106,16 +106,17 @@ class FreetrialController extends Controller
   private function lead_create_enterprise_id($request)
   {
     $validator = Validator::make($request->all(), [
-      'first_name'          => ['required', 'string'],
-      'last_name'           => ['required', 'string'],
-      'enterprise_name'     => ['required', 'string'],
-      'address'             => ['required', 'string'],
-      'email'               => ['required', 'string'],
-      'zip'                 => ['required', 'integer'],
-      'state'               => ['required', 'string'],
-      'country'             => ['required', 'string'],
-      'city'                => ['required', 'string'],
-      'npwp'                => ['required', 'integer']
+      'first_name' => ['required', 'string'],
+      'last_name' => ['required', 'string'],
+      'enterprise_name' => ['required', 'string'],
+      'address' => ['required', 'string'],
+      'email' => ['required', 'string'],
+      'zip' => ['required', 'integer'],
+      'state' => ['required', 'string'],
+      'country' => ['required', 'string'],
+      'city' => ['required', 'string'],
+      'npwp' => ['required', 'integer'],
+      'enterprise_privy_id' => ['string']
     ]);
 
     if ($validator->fails()) {
@@ -187,63 +188,63 @@ class FreetrialController extends Controller
 
   private function _setUpLeadOnCreatePayload($data)
   {
-    $payload = array(
-      'first_name'        => $data['first_name'],
-      'last_name'         => $data['last_name'],
-      'address'           => (object) array(
-        "line1"       => $data['address'],
-        "city"        => $data['city'],
-        "postal_code" => $data['zip'],
-        "state"       => $data['state'],
-        "country"     => $data['country']
-      ),
-      'email'             => $data['email'],
+    $payload = [
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'address' => (object) [
+        'line1' => $data['address'],
+        'city' => $data['city'],
+        'postal_code' => $data['zip'],
+        'state' => $data['state'],
+        'country' => $data['country']
+      ],
+      'email' => $data['email'],
       'organization_name' => $data['enterprise_name'],
-      'custom_fields'     => (object) array(
-        'Finance (PIC) Name'    => $data['first_name']." ".$data['last_name'],
+      'custom_fields' => (object) [
+        'Finance (PIC) Name' => $data['first_name'] . ' ' . $data['last_name'],
         'Finance (pic) name #1' => $data['first_name'],
-        'Last name #1'          => $data['last_name'],
-        'Company name #1'       => $data['enterprise_name'],
-        'Email #1'              => $data['email'],
-        'NPWP'                  => $data['npwp'],
-        'Enterprise ID'         => $data['enterprise_privy_id']
-      )
-    );
+        'Last name #1' => $data['last_name'],
+        'Company name #1' => $data['enterprise_name'],
+        'Email #1' => $data['email'],
+        'NPWP' => $data['npwp'],
+        'Enterprise ID' => $data['enterprise_privy_id']
+      ]
+    ];
 
     return $payload;
   }
 
   private function _setUpLeadOnChangePayload($leadID, $payload)
   {
-    $existingData   = $this->zd_lead_get((int)$leadID);
+    $existingData = $this->zd_lead_get((int)$leadID);
     if (!isset($existingData->original['data'])) {
       return false;
     }
-    $oldData        = $existingData->original['data'];
-    $first_name     = !isset($payload['first_name']) || $payload['first_name'] == "" ? $oldData['first_name']: $payload['first_name'];
-    $last_name      = !isset($payload['last_name']) || $payload['last_name'] == "" ? $oldData['last_name']: $payload['last_name'];
-    $payload        = array(
-      'first_name'        => $first_name,
-      'last_name'         => $last_name,
-      'address'           => (object) array(
-        "line1"       => !isset($payload['address']) || $payload['address'] == "" ? $oldData['address']['line1']: $payload['address'],
-        "city"        => !isset($payload['city']) || $payload['city'] == "" ? $oldData['address']['city']: $payload['city'],
-        "postal_code" => !isset($payload['zip']) || $payload['zip'] == "" ? $oldData['address']['postal_code']: $payload['zip'],
-        "state"       => !isset($payload['state']) || $payload['state'] == "" ? $oldData['address']['state']: $payload['state'],
-        "country"     => !isset($payload['country']) || $payload['country'] == "" ? $oldData['address']['country']: $payload['country'],
-      ),
-      'email'             => !isset($payload['email']) || $payload['email'] == "" ? $oldData['email']: $payload['email'],
-      'organization_name' => !isset($payload['enterprise_name']) || $payload['enterprise_name'] == "" ? $oldData['organization_name']: $payload['enterprise_name'],
-      'custom_fields'     => (object) array(
-        'Finance (PIC) Name'    => $first_name." ".$last_name,
+    $oldData = $existingData->original['data'];
+    $first_name = !isset($payload['first_name']) || $payload['first_name'] == '' ? $oldData['first_name'] : $payload['first_name'];
+    $last_name = !isset($payload['last_name']) || $payload['last_name'] == '' ? $oldData['last_name'] : $payload['last_name'];
+    $payload = [
+      'first_name' => $first_name,
+      'last_name' => $last_name,
+      'address' => (object) [
+        'line1' => !isset($payload['address']) || $payload['address'] == '' ? $oldData['address']['line1'] : $payload['address'],
+        'city' => !isset($payload['city']) || $payload['city'] == '' ? $oldData['address']['city'] : $payload['city'],
+        'postal_code' => !isset($payload['zip']) || $payload['zip'] == '' ? $oldData['address']['postal_code'] : $payload['zip'],
+        'state' => !isset($payload['state']) || $payload['state'] == '' ? $oldData['address']['state'] : $payload['state'],
+        'country' => !isset($payload['country']) || $payload['country'] == '' ? $oldData['address']['country'] : $payload['country'],
+      ],
+      'email' => !isset($payload['email']) || $payload['email'] == '' ? $oldData['email'] : $payload['email'],
+      'organization_name' => !isset($payload['enterprise_name']) || $payload['enterprise_name'] == '' ? $oldData['organization_name'] : $payload['enterprise_name'],
+      'custom_fields' => (object) [
+        'Finance (PIC) Name' => $first_name . ' ' . $last_name,
         'Finance (pic) name #1' => $first_name,
-        'Last name #1'          => $last_name,
-        'Enterprise ID'         => !isset($payload['enterprise_privy_id']) || $payload['enterprise_privy_id'] == "" ? $oldData['custom_fields']['Enterprise ID']: $payload['enterprise_privy_id'],
-        'Company name #1'       => !isset($payload['enterprise_name']) || $payload['enterprise_name'] == "" ? $oldData['custom_fields']['Company name #1']: $payload['enterprise_name'],
-        'Email #1'              => !isset($payload['email']) || $payload['email'] == "" ? $oldData['custom_fields']['Email #1']: $payload['email'],
-        'NPWP'                  => !isset($payload['npwp']) || $payload['npwp'] == "" ? $oldData['custom_fields']['NPWP']: $payload['npwp'],
-      )
-    );
+        'Last name #1' => $last_name,
+        'Enterprise ID' => !isset($payload['enterprise_privy_id']) || $payload['enterprise_privy_id'] == '' ? $oldData['custom_fields']['Enterprise ID'] : $payload['enterprise_privy_id'],
+        'Company name #1' => !isset($payload['enterprise_name']) || $payload['enterprise_name'] == '' ? $oldData['custom_fields']['Company name #1'] : $payload['enterprise_name'],
+        'Email #1' => !isset($payload['email']) || $payload['email'] == '' ? $oldData['custom_fields']['Email #1'] : $payload['email'],
+        'NPWP' => !isset($payload['npwp']) || $payload['npwp'] == '' ? $oldData['custom_fields']['NPWP'] : $payload['npwp'],
+      ]
+    ];
 
     return $payload;
   }

e9f74a144272f0a611d0fcfa6ba70476d1570176
auful01 <distriq19@gmail.com>
2024-01-26 13:53:54 +0700

    update

--- app/Http/Controllers/Zendesk/Lead/OnChangeController.php
+++ app/Http/Controllers/Zendesk/Lead/OnChangeController.php
@@ -10,39 +10,41 @@ use Illuminate\Support\Facades\Log;
 
 class OnChangeController extends Controller
 {
-  public function index(Request $request)
-  {
-    Log::debug('--- Zendesk-Event: Lead on Status Changed ---');
-    Log::debug(json_encode($request->toArray(), JSON_PRETTY_PRINT));
-    Log::debug('--- AC-Request: Update Contact Request --');
-    Log::debug(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $request->ac_contact_id);
-    $payload = [
-      'contact' => [
-        'firstName'   => $request->first_name,
-        'lastName'    => $request->last_name,
-        'fieldValues' => [
-          [
-            'field' => 5,
-            'value' => $request->status
-          ],
-          [
-            'field' => 7,
-            'value' => $request->enterprise_id
-          ]
-        ]
-      ]
-    ];
-    Log::debug(json_encode($payload, JSON_PRETTY_PRINT));
+    public function index(Request $request)
+    {
+        Log::debug('--- Zendesk-Event: Lead on Status Changed ---');
+        Log::debug(json_encode($request->toArray(), JSON_PRETTY_PRINT));
+        Log::debug('--- AC-Request: Update Contact Request --');
+        Log::debug(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $request->ac_contact_id);
+        $payload = [
+            'contact' => [
+                'firstName'   => $request->first_name,
+                'lastName'    => $request->last_name,
+                'fieldValues' => [
+                    [
+                        'field' => 5,
+                        'value' => $request->status
+                    ],
+                    [
+                        'field' => 7,
+                        'value' => $request->enterprise_id
+                    ]
+                ]
+            ]
+        ];
+        Log::debug(json_encode($payload, JSON_PRETTY_PRINT));
 
-    $response = Http::withHeaders([
-      'Api-Token' => env('ACTIVECAMPAIGN_API_KEY')
-    ])->put(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $request->ac_contact_id, $payload);
+        $response = Http::withHeaders([
+            'Api-Token' => "47b6869d496b7ad646167994d2c70efedd1e0de7a3ea86adf792ccc597501fb62ad98118",
+            'content-type' => 'application/json',
+            'accept' => 'application/json'
+        ])->put(env('ACTIVECAMPAIGN_URL') . '/api/3/contacts/' . $request->ac_contact_id, $payload);
 
-    Log::debug('--- AC-Request: Update Contact Response --');
+        Log::debug('--- AC-Request: Update Contact Response --');
 
-    $res_json = $response->json();
-    Log::debug(json_encode($res_json, JSON_PRETTY_PRINT));
+        $res_json = $response->json();
+        Log::debug(json_encode($res_json, JSON_PRETTY_PRINT));
 
-    return $this->responseOK();
-  }
+        return $this->responseOK();
+    }
 }

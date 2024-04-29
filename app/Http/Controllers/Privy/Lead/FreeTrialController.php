51a579e7ae141cd09f6a68105d729e8824549643
auful01 <distriq19@gmail.com>
2024-04-29 15:36:15 +0700

    update

--- app/Http/Controllers/Privy/Lead/FreeTrialController.php
+++ app/Http/Controllers/Privy/Lead/FreeTrialController.php
@@ -214,7 +214,7 @@ class FreetrialController extends Controller
         // Retrieve the validated input...
         $validated = $validator->validated();
         // Setup payload data
-        $payload = $this->_setUpLeadOnCreatePayload($validated);
+        $payload = $this->_setUpLeadOnCreatePayload($payload);
 
         try {
             $dataLeads = [];
